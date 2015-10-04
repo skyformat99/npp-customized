4d68df548fb8ed296c73e7ee04f972ae1c13bd12
Wudi <wudi@wudilabs.org>
2015-10-04 20:05:32 +0800

    Modified comments to avoid warning C4819
    * Modified the dash character in comments to avoid warning C4819 in code page 936.

--- PowerEditor/src/MISC/Common/mutex.cpp
+++ PowerEditor/src/MISC/Common/mutex.cpp
@@ -1,15 +1,8 @@
-// YUNI's default license is the GNU Lesser Public License (LGPL), with some
-// exclusions (see below). This basically means that you can get the full source
-// code for nothing, so long as you adhere to a few rules.
-// 
-// Under the LGPL you may use YUNI for any purpose you wish, and modify it if you
-// require, as long as you:
-// 
 // Pass on the (modified) YUNI source code with your software, with original 
 // copyrights intact :
 //  * If you distribute electronically, the source can be a separate download
 //    (either from your own site if you modified YUNI, or to the official YUNI
-//    website if you used an unmodified version) – just include a link in your
+//    website if you used an unmodified version) - just include a link in your
 //    documentation
 //  * If you distribute physical media, the YUNI source that you used to build
 //    your application should be included on that media
